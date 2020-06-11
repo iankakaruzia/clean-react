596aad820cfe82c40d71b7e737c46e9d18e026cd
Ianka Karúzia <iankakaruzia@hotmail.com>
2020-06-11 17:17:57 -0300

    test: ensure Login link goes to the right place

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -195,4 +195,12 @@ describe('Signup Component', () => {
     Helper.testElementText(sut, 'main-error', error.message)
     Helper.testChildCount(sut, 'error-wrap', 1)
   })
+
+  test('Should got to login page', async () => {
+    const { sut } = makeSut()
+    const loginLink = sut.getByTestId('login-link')
+    fireEvent.click(loginLink)
+    expect(history.length).toBe(1)
+    expect(history.location.pathname).toBe('/login')
+  })
 })
