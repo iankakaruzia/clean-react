b76f779240731b18b3e28cfbd133293fed596d3c
Ianka Karúzia <iankakaruzia@hotmail.com>
2020-06-16 21:14:04 -0300

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 404

--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -31,7 +31,17 @@ describe('RemoteLoadSurveyList', () => {
   test('Should throw UnexpectedError if HttpGetClient returns 403', async () => {
     const { sut, httpGetClientSpy } = makeSut()
     httpGetClientSpy.response = {
-      statusCode: HttpStatusCode.badRequest
+      statusCode: HttpStatusCode.forbidden
+    }
+    const promise = sut.loadAll()
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  test('Should throw UnexpectedError if HttpGetClient returns 404', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
     }
     const promise = sut.loadAll()
 
