d645c34ba367b0364bcfe439fbf23750134c93ff
Ianka Karuzia <iankakaruzia@gmail.com>
2021-01-15 19:34:38 -0300

    refactor: adapt layout to mobile

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -24,6 +24,34 @@ const SurveyList: React.FC = () => {
               Ver Resultado
             </footer>
           </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon iconName={IconName.thumbUp} className={Styles.iconWrap} />
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>04</span>
+                <span className={Styles.year}>2020</span>
+              </time>
+              <p>Qual é o seu framework web favorito?</p>
+            </div>
+            <footer>
+              Ver Resultado
+            </footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon iconName={IconName.thumbUp} className={Styles.iconWrap} />
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>04</span>
+                <span className={Styles.year}>2020</span>
+              </time>
+              <p>Qual é o seu framework web favorito?</p>
+            </div>
+            <footer>
+              Ver Resultado
+            </footer>
+          </li>
         </ul>
       </div>
       <Footer />
