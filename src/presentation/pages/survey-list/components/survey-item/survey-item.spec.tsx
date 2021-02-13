427d2d7a9b4d27e67cbeffc169a429c38e8d74d6
Ianka Karuzia <iankakaruzia@gmail.com>
2021-02-12 21:05:32 -0300

    test: ensure SurveyItem load icon and day correctly

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -4,17 +4,38 @@ import { SurveyItem } from '@/presentation/pages/survey-list/components'
 import { mockSurveyModel } from '@/domain/test'
 import { IconName } from '@/presentation/components'
 
+const makeSut = (survey = mockSurveyModel()): void => {
+  render(<SurveyItem survey={survey} />)
+}
+
 describe('SurveyItem Component', () => {
   test('Should render with correct values', () => {
-    const survey = mockSurveyModel()
-    survey.didAnswer = true
-    survey.date = new Date('2021-01-10T00:00:00')
-    render(<SurveyItem survey={survey} />)
+    const survey = {
+      ...mockSurveyModel(),
+      didAnswer: true,
+      date: new Date('2021-01-10T00:00:00')
+    }
+    makeSut(survey)
 
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
     expect(screen.getByTestId('day')).toHaveTextContent('10')
     expect(screen.getByTestId('month')).toHaveTextContent('jan')
     expect(screen.getByTestId('year')).toHaveTextContent('2021')
+  })
+
+  test('Should render with correct values', () => {
+    const survey = {
+      ...mockSurveyModel(),
+      didAnswer: false,
+      date: new Date('2020-03-08T00:00:00')
+    }
+    makeSut(survey)
+
+    expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbDown)
+    expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
+    expect(screen.getByTestId('day')).toHaveTextContent('08')
+    expect(screen.getByTestId('month')).toHaveTextContent('mar')
+    expect(screen.getByTestId('year')).toHaveTextContent('2020')
   })
 })
