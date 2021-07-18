d21e723dceb35bca2090ca106b23f757430f2f9e
Ianka Karuzia <iankakaruzia@gmail.com>
2021-07-18 18:06:41 -0300

    test: ensure RemoteLoadSurveyResult throws UnexpectedError on 500

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -47,4 +47,14 @@ describe('RemoteLoadSurveyResult', () => {
 
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  it('Should throw UnexpectedError if HttpGetClient returns 500', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.load()
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
