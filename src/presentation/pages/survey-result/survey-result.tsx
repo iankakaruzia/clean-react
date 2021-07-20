78e7072cf4692f8bd80a930ec0993c9497db315e
Ianka Karuzia <iankakaruzia@gmail.com>
2021-07-20 20:28:46 -0300

    feat: ensure SurveyResult presents correct initial state

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FlipMove from 'react-flip-move'
-import { Header, Footer, Loading, Calendar } from '@/presentation/components'
+import {
+  Header,
+  Footer,
+  Loading,
+  Calendar,
+  Error
+} from '@/presentation/components'
+import { LoadSurveyResult } from '@/domain/usecases'
 
 import Styles from './survey-result-styles.scss'
 
 const SurveyResult: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    error: '',
+    surveyResult: null as LoadSurveyResult.Model
+  })
+
   return (
     <div className={Styles.surveyResultWrap}>
       <Header />
-      <div className={Styles.contentWrap}>
-        {false && (
+      <div data-testid='survey-result' className={Styles.contentWrap}>
+        {state.surveyResult && (
           <>
             <hgroup>
               <Calendar date={new Date()} className={Styles.calendarWrap} />
@@ -42,7 +55,8 @@ const SurveyResult: React.FC = () => {
               </li>
             </FlipMove>
             <button>Voltar</button>
-            {false && <Loading />}
+            {state.isLoading && <Loading />}
+            {state.error && <Error error={state.error} reload={() => {}} />}
           </>
         )}
       </div>
